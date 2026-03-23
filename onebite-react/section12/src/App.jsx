fc11032da591cffb91eb5ee62f596a889a3abbf6
LimdaeIl <optional94@naver.com>
2026-03-23 23:36:53 +0900

    commit

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { useReducer, useRef, createContext } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
@@ -18,16 +19,97 @@ import Edit from "./pages/Edit.jsx";
 // npm run preview는 4173 으로 실행됩니다. public 이미지들의 주소는 /emotion2.png 인 반면에,
 // img 태그의 src 속성으로 가져오는 이미지 경로의 주소는 캐시 이미지명으로 됩니다.
 
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) => (String(item.id) === String(action.data.id) ? action.data : item));
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.data.id));
+    default:
+      return state;
+  }
+}
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    // 새로운 일기를 추가하는 기능
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      data: {
+        id,
+      },
+    });
+  };
+
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='*' element={<Notfound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete,
+          }}
+        >
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/diary/:id' element={<Diary />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            <Route path='*' element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
--- onebite-react/section12/src/pages/Home.jsx
+++ onebite-react/section12/src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import { useSearchParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
 
 // useSearchParams 쿼리 스트링을 처리할 떄 사용하는 리액트 돔이 있습니다.
 const Home = () => {
-  const [params, setParams] = useSearchParams();
-  console.log(params.get("value"));
-  console.log(setParams);
-
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header title={"2024년 2월"} leftChild={<Button text={"<"} />} rightChild={<Button text={">"} />} />
+      <DiaryList />
+    </div>
+  );
 };
 
 export default Home;
